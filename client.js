7446559d743b586f7d4703d05c51778fab7cf55f
Onyinye Ochi <osemene.go@gmail.com>
2023-07-05 22:17:37 +0000

    connectimg module

---
+++ client.js
@@ -0,0 +1,18 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: 165.227.47.243
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Event handler for incoming data
+  conn.on("data", function (data) {
+    console.log("Received:", data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,16 +1,6 @@
-const connect = function () {
-  const conn = net.createConnection({
-    host: 165.227.47.243
-    port: 50541,
-  });
+const { connect } = require("./client");
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+console.log("Connecting ...");
+const connection = connect();
 
-  // Event handler for incoming data
-  conn.on("data", function (data) {
-    console.log("Received:", data);
-  });
-
-  return conn;
-};
+connection.write("");
