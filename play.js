6d36652912cbf5163301f9288063be7269a54bb4
Onyinye Ochi <osemene.go@gmail.com>
2023-07-05 22:07:23 +0000

    first commit

---
+++ play.js
@@ -0,0 +1,16 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: 165.227.47.243
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Event handler for incoming data
+  conn.on("data", function (data) {
+    console.log("Received:", data);
+  });
+
+  return conn;
+};
